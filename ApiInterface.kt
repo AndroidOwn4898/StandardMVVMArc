7090ad6891ecea0fa28eb9a95499533cfe03ac2b
AndroidOwn4898 <166979562+AndroidOwn4898@users.noreply.github.com>
2024-06-07 18:01:11 +0530

    Update ApiInterface.kt

--- ApiInterface.kt
+++ ApiInterface.kt
@@ -36,63 +36,13 @@ interface ApiInterface {
     @POST("beforeauth/login")
     fun userLogin(@FieldMap request: ArrayMap<String, Any>): Call<CommonResponse<UserData>>
 
-    @POST("afterauth/update-device-token")
-    fun updateDeviceToken(): Call<CommonResponse<Any>>
-
     @POST("beforeauth/country-list")
     fun getCountryList(): Call<CommonResponse<CountryListModel>>
-
-    @POST("beforeauth/get-banner")
-    fun getLoginBanner(): Call<CommonResponse<LoginBannerListModel>>
-
-    @POST("afterauth/get-user")
-    fun getUserDetails(): Call<CommonResponse<UserDetails>>
-
-    @FormUrlEncoded
-    @POST("afterauth/logout")
-    fun logout(@FieldMap request: ArrayMap<String, String>): Call<CommonResponse<Any>>
 
     @FormUrlEncoded
     @POST("beforeauth/verification")
     fun emailVerification(@FieldMap request: ArrayMap<String, String>): Call<CommonResponse<Any>>
 
-    @FormUrlEncoded
-    @POST("beforeauth/verification")
-    fun verificationMobile(@FieldMap request: ArrayMap<String, String>): Call<CommonResponse<VerificationData>>
-
-    @FormUrlEncoded
-    @POST("beforeauth/verification")
-    fun verificationEmail(@FieldMap request: ArrayMap<String, String>): Call<CommonResponse<Any>>
-
-    @FormUrlEncoded
-    @POST("beforeauth/reset-password")
-    fun resetPassword(@FieldMap request: ArrayMap<String, String>): Call<CommonResponse<Any>>
-
-    @POST("shiftauth/get-date-shift-publisher")
-    fun getOwnerDateShiftList(@Query("shift_date") date: String): Call<CommonResponse<StaffSlotListModel>>
-
-
-    @POST("shiftauth/get-week-shift-serviceprovider")
-    fun getWeekShiftList(): Call<CommonResponse<HomeWeekListModel>>
-
-    @POST("shiftauth/get-week-shift-publisher")
-    fun getOwnerWeekShiftList(): Call<CommonResponse<HomeWeekListModel>>
-
-    @POST("shiftauth/get-home-shift-serviceprovider")
-    fun getMonthShiftList(@Query("shift_month") month: String): Call<CommonResponse<HomeWeekListModel>>
-
-    @POST("shiftauth/get-home-shift-publisher")
-    fun getOwnerMonthShiftList(@Query("shift_month") month: String): Call<CommonResponse<HomeWeekListModel>>
-
-    @FormUrlEncoded
-    @POST("beforeauth/signup")
-    fun signup(@FieldMap request: ArrayMap<String, Any>): Call<CommonResponse<UserData>>
-
-    @POST("afterauth/get-profile")
-    fun getProfile(): Call<CommonResponse<ProfileList>>
-
-    @POST("beforeauth/province-list")
-    fun getProvince(@Query("country_id") countryId: String): Call<CommonResponse<ProvinceListModel>>
 
     @POST("afterauth/category-list")
     fun getCategory(
@@ -101,67 +51,6 @@ interface ApiInterface {
         @Query("module_id") moduleId: String
     ): Call<CommonResponse<CategoryListModel>>
 
-    @FormUrlEncoded
-    @POST("afterauth/add-profile")
-    fun addOwnerProfile(@FieldMap request: ArrayMap<String, Any>): Call<CommonResponse<Any>>
-
-    @FormUrlEncoded
-    @POST("afterauth/edit-profile")
-    fun editProfile(@FieldMap request: ArrayMap<String, Any>): Call<CommonResponse<Any>>
-
-    @POST("afterauth/language-list")
-    fun getLanguageList(@Query("category_id") categoryId: String): Call<CommonResponse<LanguageModel>>
-
-    @POST("afterauth/software-list")
-    fun getSoftwareList(@Query("category_id") categoryId: String): Call<CommonResponse<SoftwareModel>>
-
-    @POST("afterauth/skill-list")
-    fun getSkillList(@Query("category_id") categoryId: String): Call<CommonResponse<SkillModel>>
-
-    @FormUrlEncoded
-    @POST("s3bucketauth/get-document")
-    fun getDocument(@FieldMap request: ArrayMap<String, Any>): Call<CommonResponse<ProfileDocumentModel>>
-
-    @Multipart
-    @POST("s3bucketauth/upload-file")
-    fun uploadFile(
-        @PartMap request: HashMap<String, RequestBody>?,
-        @Part imageFile: MultipartBody.Part?,
-    ): Call<CommonResponse<UploadDocumentModel>>
-
-    @FormUrlEncoded
-    @POST("s3bucketauth/save-file")
-    fun saveFile(@FieldMap request: ArrayMap<String, Any>): Call<CommonResponse<ProfileDocumentModel>>
-
-    @POST("shiftauth/get-date-shift-serviceprovider")
-    fun getShiftServiceProviderData(@Query("shift_year") shiftYear: String): Call<CommonResponse<StaffSlotListModel>>
-
-    @FormUrlEncoded
-    @POST("beforeauth/support")
-    fun helpSupport(@FieldMap request: ArrayMap<String, String>): Call<CommonResponse<Any>>
-
-    @FormUrlEncoded
-    @POST("afterauth/support")
-    fun categoryNotList(@FieldMap request: ArrayMap<String, String>): Call<CommonResponse<Any>>
-
-    @FormUrlEncoded
-    @POST("afterauth/capture")
-    fun takeScreenShoot(@FieldMap request: ArrayMap<String, String>): Call<CommonResponse<Any>>
-
-    @POST("afterauth/get-support-list")
-    fun getSupportList(): Call<CommonResponse<SupportListModel>>
-
-    @FormUrlEncoded
-    @POST("afterauth/get-support-history")
-    fun getSupportHistoryList(@FieldMap request: ArrayMap<String, String>): Call<CommonResponse<SupportHistoryListModel>>
-
-    @FormUrlEncoded
-    @POST("afterauth/support")
-    fun addSupportTicket(@FieldMap request: ArrayMap<String, String>): Call<CommonResponse<Any>>
-
-    @FormUrlEncoded
-    @POST("afterauth/add-support-history")
-    fun addSupportHistory(@FieldMap request: ArrayMap<String, String>): Call<CommonResponse<Any>>
-
+  
 }
 
