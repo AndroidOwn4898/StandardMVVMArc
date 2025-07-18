3ad4c331cad7ccbee60b33bf6ffcaa0f932a33ea
AndroidOwn4898 <166979562+AndroidOwn4898@users.noreply.github.com>
2025-07-18 13:06:53 +0530

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -7,11 +7,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.mapFragment) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
 
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MapFragment())
-                .commitNow()
+    override fun onMapReady(googleMap: GoogleMap) {
+        val center = LatLng(23.0300, 72.5800)
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(center, 13f))
+
+        val fuelStations = listOf(
+            LatLng(23.0225, 72.5714) to "Shell Fuel Station",
+            LatLng(23.0300, 72.5800) to "HP Petrol Pump",
+            LatLng(23.0350, 72.5650) to "Indian Oil",
+            LatLng(23.0250, 72.5500) to "Reliance Pump"
+        )
+
+        for ((location, name) in fuelStations) {
+            googleMap.addMarker(MarkerOptions().position(location).title(name))
         }
     }
 }
